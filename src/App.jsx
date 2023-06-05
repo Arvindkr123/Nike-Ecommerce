28aa29c580a6e64404a4c994facd79b3c7e0c60d
Arvind <thakurarvindkr10@gmail.com>
2023-06-05 23:40:48 +0530

    added start up nike ecommerce

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-6xl text-red-600">
+        Welcome to Nike Ecommerce Website
+      </h1>
+    </div>
+  );
+};
+
+export default App;
